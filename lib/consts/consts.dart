47c3f33bf8d90c8ed4913b1588d089e0ae313af3
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-09 21:52:52 +0530

    firebase const file added

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -2,5 +2,6 @@ export './colors.dart';
 export './images.dart';
 export './strings.dart';
 export './styles.dart';
+export './firebase_consts.dart';
 export 'package:velocity_x/velocity_x.dart';
 export 'package:flutter/material.dart';
