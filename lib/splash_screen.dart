25aee5fc174982b88ea1aa9f2ef21a1eac968faa
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-11 00:53:17 +0530

    minor change

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   changeScreen(){
     Future.delayed(const Duration(seconds: 3),(){
-      Get.to(() =>  const SignInEmail());
+      Get.to(() => SignInPage());
     });
   }
 
