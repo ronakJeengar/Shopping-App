6ddb801c8facecd5e08444a30dfc2c3c5c2aeedd
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-12 00:42:20 +0530

    update profile and profile image function added

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecom/consts/consts.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:path/path.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfileController extends GetxController{
 
   RxString profileImgPath = ''.obs;
+  dynamic profileImageLink = '';
+  RxBool isLoading = false.obs;
+
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
@@ -17,6 +25,24 @@ class ProfileController extends GetxController{
       }on PlatformException catch(e){
           VxToast.show(context, msg : e.toString());
       }
+  }
+
+  uploadProfileImage() async{
+    dynamic filename = basename(profileImgPath.value);
+    dynamic destination = 'images/${currentUser!.uid}/$filename';
+    Reference ref = FirebaseStorage.instance.ref().child(destination);
+    await ref.putFile(File(profileImgPath.value));
+    profileImageLink = await ref.getDownloadURL();
+  }
+
+  updateProfile({username, password, imageUrl}) async{
+    dynamic store = firestore.collection('users').doc(currentUser!.uid);
+    await store.set({
+      'username': username,
+      'password': password,
+      'imageUrl': imageUrl
+    }, SetOptions(merge: true));
+    isLoading(false);
   }
 
 }
