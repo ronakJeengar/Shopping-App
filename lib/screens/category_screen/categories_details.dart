95477a20e69de2881f7e2965c6745e6fa2246910
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-12 05:47:05 +0530

    added product controller and data from json

--- lib/screens/category_screen/categories_details.dart
+++ lib/screens/category_screen/categories_details.dart
@@ -1,5 +1,6 @@
 import 'package:ecom/common_widgets/bg.dart';
 import 'package:ecom/consts/consts.dart';
+import 'package:ecom/controller/product_controller.dart';
 import 'package:ecom/screens/category_screen/component/sub_categories.dart';
 import 'package:ecom/screens/category_screen/item_details.dart';
 import 'package:ecom/screens/home_screen/component/featured_products.dart';
@@ -7,62 +8,69 @@ import 'package:get/get.dart';
 
 class CategoriesDetails extends StatelessWidget {
   final String? title;
-  final List<List<dynamic>> subCategory;
 
-  const CategoriesDetails(
-      {super.key, required this.title, required this.subCategory});
+  const CategoriesDetails({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
+    final ProductController productController = Get.find<ProductController>();
+
     return bg(
         child: Scaffold(
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.white, // Change the color here
-        ),
-        title: title!.text.fontFamily(bold).white.make(),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:
-                    List.generate(4, (index) => subCategories(text: title)),
-              ),
+          appBar: AppBar(
+            iconTheme: const IconThemeData(
+              color: Colors.white, // Change the color here
             ),
-            10.heightBox,
-            Expanded(
-                child: Container(
-              child: SingleChildScrollView(
-                child: GridView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: 6,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 8,
-                        crossAxisSpacing: 8,
-                        mainAxisExtent: context.screenHeight * 0.30),
-                    itemBuilder: (context, index) {
-                      return featuredProduct(
-                          image: imgP5,
-                          height: 250,
-                          width: 200,
-                          title: 'Bag',
-                          price: '\$400').onTap(() {
-                        Get.to(() => const ItemDetails(title: 'Bag'));
-                      });
-                    }),
-              ),
-            ))
-          ],
-        ),
-      ),
-    ));
+            title: title!
+                .text
+                .fontFamily(bold)
+                .white
+                .make(),
+          ),
+          body: Container(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:
+                    List.generate(productController.subCat.length, (index) =>
+                        Text(
+                          productController.subCat[index],
+                        ).box.roundedSM.alignCenter.white.size(150, 60).margin(
+                            const EdgeInsets.symmetric(horizontal: 4.0)).make()),
+                  ),
+                ),
+                10.heightBox,
+                Expanded(
+                    child: SingleChildScrollView(
+                      child: GridView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: 6,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 8,
+                              crossAxisSpacing: 8,
+                              mainAxisExtent: context.screenHeight * 0.30),
+                          itemBuilder: (context, index) {
+                            return featuredProduct(
+                                image: imgP5,
+                                height: 250,
+                                width: 200,
+                                title: 'Bag',
+                                price: '\$400').onTap(() {
+                              Get.to(() => const ItemDetails(title: 'Bag'));
+                            });
+                          }),
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
