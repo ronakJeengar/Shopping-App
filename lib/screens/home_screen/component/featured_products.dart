449f04c10fb0fb32a4534ad15fb12f06fdc0e9d9
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-14 04:15:14 +0530

    added new method for static images

--- lib/screens/home_screen/component/featured_products.dart
+++ lib/screens/home_screen/component/featured_products.dart
@@ -1,6 +1,40 @@
 import 'package:ecom/consts/consts.dart';
 
-Widget featuredProduct({
+Widget featuredProductNetwork({
+  required String? image,
+  required double? height,
+  required double? width,
+  required String? title,
+  required String? price}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Image.network(
+        image!,
+        height: height,
+        width: width,
+        fit: BoxFit.fill,
+      ),
+      10.widthBox,
+      Text(
+        title!,
+      ).text.color(darkFontGrey).fontFamily(semibold).make(),
+      10.widthBox,
+      Text(
+        price!,
+      ).text.color(redColor).size(16).fontFamily(bold).make(),
+    ],
+  )
+      .box
+      .white
+      .margin(const EdgeInsets.symmetric(horizontal: 4.0))
+      .padding(const EdgeInsets.all(8.0))
+      .roundedSM
+      .outerShadowSm
+      .make();
+}
+
+Widget featuredProductAsset({
   required String? image,
   required double? height,
   required double? width,
@@ -22,7 +56,7 @@ Widget featuredProduct({
       10.widthBox,
       Text(
         price!,
-      ).text.color(redColor).size(16).fontFamily(bold).make()
+      ).text.color(redColor).size(16).fontFamily(bold).make(),
     ],
   )
       .box
