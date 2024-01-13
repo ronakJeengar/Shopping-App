19d18baf09c4dec38567bc9e0c7b7ccafefa9de7
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-14 04:13:20 +0530

    get product method added

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -1,10 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecom/consts/firebase_consts.dart';
 
-class FirestoreServices{
-
-  static getUser(uid){
-    return firestore.collection('users').where('id', isEqualTo: uid).snapshots();
+class FirestoreServices {
+  //get user method
+  static getUser(uid) {
+    return firestore
+        .collection('users')
+        .where('id', isEqualTo: uid)
+        .snapshots();
   }
 
+  //get products
+  static getProducts(category) {
+    return firestore
+        .collection('products')
+        .where('product_category', isEqualTo: category)
+        .snapshots();
+  }
 }
