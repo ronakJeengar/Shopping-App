1aa48bca095c3f4fc6e15a073e0f71c0746fb9ff
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-15 00:50:23 +0530

    exit dialouge box added

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:ecom/screens/cart_screen/cart_screen.dart';
 import 'package:ecom/screens/category_screen/category_screen.dart';
 import 'package:ecom/screens/home_screen/home_screen.dart';
 import 'package:ecom/screens/profile_screen/profile_screen.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class Home extends StatefulWidget {
@@ -33,28 +34,63 @@ class _HomeState extends State<Home> {
     const Cart(), Profile()
   ];
 
+  Future<bool?> _showExitDialog(BuildContext context) async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false); // Close the AlertDialog
+            },
+            child: const Text('No'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pop(true); // Return true when 'Yes' is pressed
+              SystemNavigator.pop();
+            },
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-          children: [
-            Obx(() => Expanded(
-                child:
-                    navItemScreen.elementAt(homeController.currentIndex.value)))
-          ],
-        ),
-        bottomNavigationBar: Obx(
-          () => BottomNavigationBar(
-            currentIndex: homeController.currentIndex.value,
-            selectedItemColor: redColor,
-            selectedLabelStyle: const TextStyle(fontFamily: semibold),
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.white,
-            items: _barItems,
-            onTap: (value) {
-              homeController.currentIndex.value = value;
-            },
+    return PopScope(
+      canPop: false,
+
+        onPopInvoked: (bool didPop) async {
+          if (didPop) {
+            return;
+          }
+          await _showExitDialog(context);
+        },
+      child: Scaffold(
+          body: Column(
+            children: [
+              Obx(() => Expanded(
+                  child:
+                  navItemScreen.elementAt(homeController.currentIndex.value)))
+            ],
           ),
-        ));
+          bottomNavigationBar: Obx(
+                () => BottomNavigationBar(
+              currentIndex: homeController.currentIndex.value,
+              selectedItemColor: redColor,
+              selectedLabelStyle: const TextStyle(fontFamily: semibold),
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.white,
+              items: _barItems,
+              onTap: (value) {
+                homeController.currentIndex.value = value;
+              },
+            ),
+          ))
+    );
   }
 }
