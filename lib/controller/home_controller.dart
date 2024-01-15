951d9bf805e7389e6e099e7cdcfd6207de66da5f
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-16 01:34:31 +0530

    getusername method added

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,7 +1,17 @@
+import 'package:ecom/consts/consts.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController{
 
   var currentIndex = 0.obs;
+  dynamic username = '';
 
+  getUsername() async {
+    dynamic name = await firestore.collection('users').where('id', isEqualTo: currentUser!.uid).get().then((value) {
+      if(value.docs.isNotEmpty){
+        return value.docs.single['username'];
+      }
+    });
+    username = name;
+  }
 }
