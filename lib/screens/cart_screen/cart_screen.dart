2e0934611c887ce36247104841b83ad192e1f609
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-16 01:28:38 +0530

    delete method called on delete button

--- lib/screens/cart_screen/cart_screen.dart
+++ lib/screens/cart_screen/cart_screen.dart
@@ -41,74 +41,73 @@ class _CartState extends State<Cart> {
             );
           } else {
             var data = snapshot.data!.docs;
+            cartController.calculateTotalPrice(data);
             return Padding(
               padding: const EdgeInsets.all(12.0),
               child: Column(
-                  children: [
-                    Expanded(
-                        child: ListView.builder(
-                            itemCount: data.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                leading:
-                                    Image.network("${data[index]['image']}"),
-                                title: "${data[index]['title']}"
-                                    .text
-                                    .fontFamily(semibold)
-                                    .size(16)
-                                    .make(),
-                                subtitle: "\$ ${data[index]['totalPrice']}"
-                                    .text
-                                    .size(16)
-                                    .color(redColor)
-                                    .fontFamily(semibold)
-                                    .make(),
-                                trailing: IconButton(
-                                  onPressed: () {},
-                                  icon:
-                                      const Icon(Icons.delete, color: redColor),
-                                ),
-                              );
-                            })),
-                    Obx(() =>
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          "Total Price"
-                              .text
-                              .color(darkFontGrey)
-                              .fontFamily(semibold)
-                              .make(),
-                          cartController
-                              .calculateTotalPrice(data)
-                              .text
-                              .color(redColor)
-                              .fontFamily(semibold)
-                              .make()
-                        ],
-                      )
-                          .box
-                          .padding(const EdgeInsets.all(12.0))
-                          .width(context.screenWidth - 60)
-                          .color(lightGrey)
-                          .roundedSM
-                          .make(),
-                    ),
-                    10.heightBox,
-                    SizedBox(
-                      width: (context.screenWidth - 60),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: redColor,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0))),
-                        child: const Text("Shipping"),
-                      ),
+                children: [
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              leading: Image.network("${data[index]['image']}"),
+                              title: "${data[index]['title']} (x${data[index]['qty']})"
+                                  .text
+                                  .fontFamily(semibold)
+                                  .size(16)
+                                  .make(),
+                              subtitle: "\$ ${data[index]['totalPrice']}"
+                                  .text
+                                  .size(16)
+                                  .color(redColor)
+                                  .fontFamily(semibold)
+                                  .make(),
+                              trailing: IconButton(
+                                onPressed: () {
+                                  FirestoreServices.deleteProduct(data[index].id);
+                                },
+                                icon: const Icon(Icons.delete, color: redColor),
+                              ),
+                            );
+                          })),
+                  Obx(
+                    () => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        "Total Price"
+                            .text
+                            .color(darkFontGrey)
+                            .fontFamily(semibold)
+                            .make(),
+                        cartController.totalPrice.value.text
+                            .color(redColor)
+                            .fontFamily(semibold)
+                            .make()
+                      ],
                     )
-                  ],
-                ),
+                        .box
+                        .padding(const EdgeInsets.all(12.0))
+                        .width(context.screenWidth - 60)
+                        .color(lightGrey)
+                        .roundedSM
+                        .make(),
+                  ),
+                  10.heightBox,
+                  SizedBox(
+                    width: (context.screenWidth - 60),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: redColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0))),
+                      child: const Text("Shipping"),
+                    ),
+                  )
+                ],
+              ),
             );
           }
         },
