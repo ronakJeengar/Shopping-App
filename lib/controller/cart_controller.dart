71111da5588b46e2e65111370a31c342625e6533
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-16 01:27:25 +0530

    few changes in method

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -3,10 +3,10 @@ import 'package:get/get.dart';
 class CartController extends GetxController {
   var totalPrice = 0.obs;
 
-  int calculateTotalPrice(data) {
+  calculateTotalPrice(data) {
+    totalPrice.value = 0;
     for (int i = 0; i < data.length; i++) {
-      totalPrice = totalPrice + int.parse(data[i]['totalPrice'].toString());
+      totalPrice.value = totalPrice.value + int.parse(data[i]['totalPrice'].toString());
     }
-    return totalPrice.value;
   }
 }
