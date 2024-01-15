38ef60ce83e667bbe41a39f75bf3a67b44ddf787
Ronak Jeengar <ronakjeengar7860@gmail.com>
2024-01-15 23:40:12 +0530

    cart controller added

--- lib/screens/cart_screen/cart_screen.dart
+++ lib/screens/cart_screen/cart_screen.dart
@@ -1,13 +1,118 @@
 import 'package:ecom/consts/consts.dart';
+import 'package:ecom/controller/cart_controller.dart';
+import 'package:ecom/services/firestore_services.dart';
+import 'package:get/get.dart';
 
-class Cart extends StatelessWidget {
+class Cart extends StatefulWidget {
   const Cart({super.key});
 
   @override
+  State<Cart> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  final CartController cartController = Get.find<CartController>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: "Cart is empty".text.fontFamily(bold).size(16).make(),
+    return Scaffold(
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: whiteColor,
+        title: "Shopping Cart"
+            .text
+            .color(darkFontGrey)
+            .fontFamily(semibold)
+            .make(),
+      ),
+      body: StreamBuilder(
+        stream: FirestoreServices.getCart(currentUser!.uid),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(redColor),
+              ),
+            );
+          } else if (snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: "No items found in cart".text.color(darkFontGrey).make(),
+            );
+          } else {
+            var data = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                  children: [
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: data.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                leading:
+                                    Image.network("${data[index]['image']}"),
+                                title: "${data[index]['title']}"
+                                    .text
+                                    .fontFamily(semibold)
+                                    .size(16)
+                                    .make(),
+                                subtitle: "\$ ${data[index]['totalPrice']}"
+                                    .text
+                                    .size(16)
+                                    .color(redColor)
+                                    .fontFamily(semibold)
+                                    .make(),
+                                trailing: IconButton(
+                                  onPressed: () {},
+                                  icon:
+                                      const Icon(Icons.delete, color: redColor),
+                                ),
+                              );
+                            })),
+                    Obx(() =>
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          "Total Price"
+                              .text
+                              .color(darkFontGrey)
+                              .fontFamily(semibold)
+                              .make(),
+                          cartController
+                              .calculateTotalPrice(data)
+                              .text
+                              .color(redColor)
+                              .fontFamily(semibold)
+                              .make()
+                        ],
+                      )
+                          .box
+                          .padding(const EdgeInsets.all(12.0))
+                          .width(context.screenWidth - 60)
+                          .color(lightGrey)
+                          .roundedSM
+                          .make(),
+                    ),
+                    10.heightBox,
+                    SizedBox(
+                      width: (context.screenWidth - 60),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: redColor,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                        child: const Text("Shipping"),
+                      ),
+                    )
+                  ],
+                ),
+            );
+          }
+        },
+      ),
     );
   }
 }
